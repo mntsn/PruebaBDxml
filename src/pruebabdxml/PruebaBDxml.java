09bc0aaa262e39b30f74f688068dbd1b6973f879
mntsn <44491386+mntsn@users.noreply.github.com>
2018-10-26 18:14:54 +0200

    Revert "Sin exito aun"
    This reverts commit 70ec3aa465edf475a2ae3d7684a943e94c6ce165.

--- src/pruebabdxml/PruebaBDxml.java
+++ src/pruebabdxml/PruebaBDxml.java
@@ -3,27 +3,26 @@ package pruebabdxml;
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.Marshaller;
 import java.io.File;
-import java.util.List;
 import java.util.ArrayList;
 
 
 public class PruebaBDxml {
      public static void main(String[] args) {
-            
-            Usuario user1 = new Usuario ("jose","fsededf.es","Bami3","bami3");
-            Usuario user2 = new Usuario ("pedro","fsdf.es","Bami4","bami6");
-            Usuarios listaUsuarios = new Usuarios();
-            listaUsuarios.add(user1);
+         ArrayList <Usuario> listaGente = new ArrayList(); //Array no sirve
+             Usuario user1 = new Usuario ("jose","fsededf.es","Bami3","bami3");
+             Usuario user2 = new Usuario ("pedro","fsdf.es","Bami4","bami6");
+             listaGente.add(user1);
+             listaGente.add(user2);//no he encontrado manera de imprimir este en xml
          try{
-            JAXBContext contexto = JAXBContext.newInstance(Usuarios.getClass());// por que user1?
-            File file = new File ("parking.xml");
-            Marshaller marshaller = contexto.createMarshaller();
-            marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
-            marshaller.marshal(listaUsuarios, file);//como imprimir toda la lista?
+             JAXBContext contexto = JAXBContext.newInstance(user1.getClass());// por que user1?
+             File file = new File ("parking.xml");
+             Marshaller marshaller = contexto.createMarshaller();
+             marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
+             marshaller.marshal(user1, file);//como imprimir toda la lista?
              
          } 
          catch (javax.xml.bind.JAXBException ex) {
-            java.util.logging.Logger.getLogger("global").log(java.util.logging.Level.SEVERE,null,ex);
+             java.util.logging.Logger.getLogger("global").log(java.util.logging.Level.SEVERE,null,ex);
          } 
         
          
